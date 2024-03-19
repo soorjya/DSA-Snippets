b25fa0fb8fd3adc7063e32cd774a59022150f5b7
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2024-03-19 23:51:33 +0530

    Update duplicate.cpp

--- duplicate.cpp
+++ duplicate.cpp
@@ -4,35 +4,32 @@ using namespace std;
 vector<int> duplicates(int arr[], int n)
 {
 
-    // Increment array elements by 1
+    
     for (int i = 0; i < n; i++) {
         arr[i] += 1;
     }
 
-    // result vector
+  
     vector<int> res;
 
-    // count variable for count of
-    // largest element
     int count = 0;
 
     for (int i = 0; i < n; i++) {
 
-        // Calculate index value
+        
         int index = abs(arr[i]) > n ? abs(arr[i]) / (n + 1)
                                     : abs(arr[i]);
 
-        // Check if index equals largest element value
+        
         if (index == n) {
             count++;
             continue;
         }
 
-        // Get element value at index
+       
         int val = arr[index];
 
-        // Check if element value is negative, positive
-        // or greater than n
+        
         if (val < 0) {
             res.push_back(index - 1);
             arr[index] = abs(arr[index]) * (n + 1);
@@ -43,7 +40,7 @@ vector<int> duplicates(int arr[], int n)
             arr[index] = -arr[index];
     }
 
-    // If largest element occurs more than once
+    
     if (count > 1)
         res.push_back(n - 1);
 
@@ -55,7 +52,7 @@ vector<int> duplicates(int arr[], int n)
     return res;
 }
 
-// Driver Code
+
 int main()
 {
     int numRay[] = { 0, 4, 3, 2, 7, 8, 2, 3, 1 };
