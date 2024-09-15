f37a236b2cc0a8cc96bba543b1248e0b0a5fdf41
Soorjyakanta Sethy <soorjyakant17@gmail.com>
2024-09-15 23:05:34 +0530

    Create accq3.cpp

---
+++ accq3.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int substr(string a, string b){
+    int n = a.size();
+    int m = b.size();
+    string m;
+    int i = 0,j=0;
+    while(n>0){
+        for(int i=0;i<n;i++){
+            if(a[i]==b[j])
+            m=a[i];
+        }
+    }
+}
+
+
+int main(){
+    string a,b;
+    cin>>a;
+    cin>>b;
+    return 0;
+}
